5c1e98637421cf7560411fae54c223b2f432d5d8
zxx <2665312447@qq.com>
2020-10-02 17:06:48 +0800

    [cv调包侠] 更新，您可以在公众号：DeepAI 视界中回复香烟领取数据，解压后放入data，直接train

--- voc_label.py
+++ voc_label.py
@@ -1,8 +1,9 @@
-# 坐标xml转txt
+# 缺陷坐标xml转txt
 import os
 import xml.etree.ElementTree as ET
-
-classes = ["smoke"]  # 输入名称，必须与xml标注名称一致
+import os
+import random
+classes = ["smoke"]  # 输入类别名称，必须与xml标注名称一致
 
 def convert(size, box):
     print(size, box)
@@ -21,9 +22,8 @@ def convert(size, box):
 
 def convert_annotation(image_id):
     print(image_id)
-    in_file = open(r'./data/Annotations/%s.xml' % (image_id), 'rb')  # 读取xml文件路径
-
-    out_file = open('./data/labels/%s.txt' % (image_id), 'w')  # 需要保存的txt格式文件路径
+    in_file = open(r'./data/Annotations/%s' % (image_id), 'rb')  # 读取xml文件路径
+    out_file = open('./data/labels/%s.txt' % (image_id.split('.')[0]), 'w')  # 需要保存的txt格式文件路径
     tree = ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
@@ -32,7 +32,7 @@ def convert_annotation(image_id):
 
     for obj in root.iter('object'):
         cls = obj.find('name').text
-        if cls not in classes:  # 检索xml中的名称
+        if cls not in classes:
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
@@ -42,9 +42,32 @@ def convert_annotation(image_id):
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
 
-image_ids_train = open('./ImageXML.txt').read().strip().split()  # 读取xml文件名索引
+image_ids_train = os.listdir('./data/Annotations')  # 读取xml文件名索引
 
 for image_id in image_ids_train:
     print(image_id)
     convert_annotation(image_id)
 
+
+trainval_percent = 0.1  # 可自行进行调节
+train_percent = 1
+xmlfilepath = './data/images'
+total_xml = os.listdir(xmlfilepath)
+num = len(total_xml)
+list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+ftest = open('./data/test.txt', 'w')
+ftrain = open('./data/train.txt', 'w')
+
+for i in list:
+    name = total_xml[i] + '\n'
+    if i in trainval:
+        if i in train:
+            ftest.write('data/images/' + name)
+    else:
+        ftrain.write('data/images/' + name)
+ftrain.close()
+ftest.close()
